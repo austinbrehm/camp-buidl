b9544d0d118366ed1db003c909e5557e1c1b5b1b
Austin Brehm <austin@Austins-MacBook-Pro.local>
2025-02-23 14:49:12 -0700

    update NFT contract

--- nft.sol
+++ nft.sol
@@ -5,12 +5,12 @@ contract NFT{
     // Initialize state variables.
     struct Monster{
         uint power;
-        uint defense;
+        uint defense;  
         bool isEvil;
     }
     
-    mapping (uint => address) idToOwner;
-    mapping (uint => Monster) monsters;
+    mapping (uint => address) idToOwner;    // Store the owner of the NFT.
+    mapping (uint => Monster) monsters;    // Store the NFT data. 
     uint counter = 0;
 
     // Create a function to mint an NFT. 
@@ -35,13 +35,35 @@ contract NFT{
         idToOwner[id] = to;
     }
 
-    // Create a function to update NFT data. 
-    function update(uint id, uint _power, uint _defense, bool _isEvil) external{
-        monsters[id] = Monster(_power, _defense, _isEvil);
-    }
-
-    // Get the NFT.
+    // Create a function to get the NFT.
     function getMonster(uint id) external view returns(Monster memory){
             return monsters[id];
+    }
+
+    // Create a function to update NFT data for power attribute.
+    function updatePower(uint id, uint _power) external{
+        // Check if the sender owns the NFT.
+        require(idToOwner[id] == msg.sender, "You do not own this NFT");
+
+        // Update the power attribute.
+        monsters[id].power = _power; 
+    }
+
+    // Create a function to update NFT data for defense attribute.
+    function updateDefense(uint id, uint _defense) external{
+        // Check if the sender owns the NFT.counter
+        require(idToOwner[id] == msg.sender, "You do not own this NFT");
+
+        // Update the defense attribute.
+        monsters[id].defense = _defense;
+    }
+
+    // Create a function to update NFT data for isEvil attribute. 
+    function updateIsEvil(uint id, bool _isEvil) external{
+        // Check if the sender owns the NFT.
+        require(idToOwner[id] == msg.sender, "You do not own this NFT");
+
+        // Update the isEvil attribute.
+        monsters[id].isEvil = _isEvil;
     }
 }
